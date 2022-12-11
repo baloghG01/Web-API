0d53c392d5797d0aed52349480d40ebf2acd9891
baloghG01 <busnes14@gmail.com>
2022-12-12 00:14:37 +0100

    Employee.js: Fetched data from API

--- WebAPi/my-app/src/Employee.js
+++ WebAPi/my-app/src/Employee.js
@@ -1,11 +1,57 @@
 import React, {Component} from 'react';
+import { Table } from 'react-bootstrap';
 
 export  class Employee extends Component
 {
+    constructor(props)
+    {
+        super(props)
+        this.state={emps:[]}
+    }
+
+    refresList()
+    {
+        fetch('http://127.0.0.1:8000/employee')
+        .then(response => response.json())
+        .then(data =>{
+            this.setState({emps:data});
+        })
+    }
+
+    componentDidMount(){
+        this.refresList();
+    }
+
+    componentDidUpdate(){
+        this.refresList();
+    }
     render()
     {
-        return <div className = "mt-5 d-flex justify-content-left"> This is Employee page</div>
+        const {emps}=this.state;
+        return <div>
+            <Table className='mt-4' striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Department </th>
+                        <th>Date of join</th>
+                        <th>Photo name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {emps.map(ep =>
+                        <tr key={ep.EmployeeId}>
+                            <td>{ep.EmployeeId}</td>
+                            <td>{ep.EmployeeName}</td>
+                            <td>{ep.DepartmentName}</td>
+                            <td>{ep.DataOfJoining}</td>
+                            <td>{ep.PhotoFileName}</td>
 
+                        </tr>)}
+                </tbody>
+            </Table>
+        </div>
         
     }
 }
