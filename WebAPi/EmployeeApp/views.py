0f67cc221c37c35593a144f90e8504938bbfdba6
baloghG01 <busnes14@gmail.com>
2022-12-15 22:51:40 +0100

    views.py: Added token class

--- WebAPi/EmployeeApp/views.py
+++ WebAPi/EmployeeApp/views.py
@@ -16,9 +16,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        # Add custom claims
-        token['name'] = user.name
-        # ...
+        token['username'] = user.username
 
         return token
 
