5e4b19774f56f2be63c0293c5af2271f12aab9a1
baloghG01 <busnes14@gmail.com>
2022-12-15 22:50:03 +0100

    url.py: Added login and refresh paths

--- WebAPi/EmployeeApp/urls.py
+++ WebAPi/EmployeeApp/urls.py
@@ -1,10 +1,19 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from EmployeeApp import views
 
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import MyTokenObtainPairSerializer
+from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns=[
+
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     re_path(r'^department$',views.departmentApi),
     re_path(r'^department/([0-9]+)$',views.departmentApi),
 
