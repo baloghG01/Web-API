4757cb54117e41333af7725d41c920d2e0be0bbf
baloghG01 <busnes14@gmail.com>
2022-12-15 23:10:48 +0100

    Home.js: Added nav bar

--- WebAPi/my-app/src/Home.js
+++ WebAPi/my-app/src/Home.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react';
+import {Navigation} from './Navigation';
 
 export class Home extends Component{
 
     render(){
         return(
-            <div className="mt-5 d-flex justify-content-left">
+           
+            <div>
+                <Navigation></Navigation>
                 This is Home page.
             </div>
         )
